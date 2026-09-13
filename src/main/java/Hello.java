d8ab4d7aae859b02604431bd9b8b1b835418d2df
Jaydeep <jsonwane34@gmail.com>
2026-09-13 22:31:34 +0530

    Add Maven Jenkins project

---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven Project");
+        System.out.println("This project will be built by Jenkins");
+    }
+}
